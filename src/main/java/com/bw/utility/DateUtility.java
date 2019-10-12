38a680542a0a250c447be1f390bb1e75de6cd132
duanmenghuan <1482996478@qq.com>
2019-10-12 11:07:05 +0800

    完善了时间的工具类

--- src/main/java/com/bw/utility/DateUtility.java
+++ src/main/java/com/bw/utility/DateUtility.java
@@ -1,5 +1,6 @@
 package com.bw.utility;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -35,5 +36,72 @@ public class DateUtility {
 		}
 		return age;
 	}
+	
+	/**
+	 * 
+	 * @Title: isToday
+	 * @Description: TODO(判断是否为当天)
+	 * @param date
+	 * @return
+	 */
+	public static boolean isToday(Date date){
+		 SimpleDateFormat smt = new SimpleDateFormat("yyyyMMdd");
+		 String dateStr = smt.format(date);
+		 String todayStr = smt.format(new Date());
+		return dateStr.equals(todayStr);
+	}
 
+	/**
+	 * 
+	 * @Title: isThisYear
+	 * @Description: TODO(判断是否是今年)
+	 * @param date
+	 * @return
+	 */
+	public static boolean isThisYear(Date date){
+		 SimpleDateFormat smt = new SimpleDateFormat("yyyy");
+		 String dateStr = smt.format(date);
+		 String todayStr = smt.format(new Date());
+		 return dateStr.equals(todayStr);	
+	}
+	/**
+	 * 
+	 * @Title: getMonthStart
+	 * @Description: TODO( 给定时间对象，初始化到该月初的1日0时0分0秒0毫秒)
+	 * @param date
+	 * @return
+	 */
+	public static Date getMonthStart(Date date){
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.HOUR, 0);
+		cal.set(Calendar.AM_PM,Calendar.AM);
+		cal.set(Calendar.DAY_OF_MONTH,1);
+		return cal.getTime();
+	}
+	/**
+	 * 
+	 * @Title: getMonthEnd
+	 * @Description: TODO(求月末)
+	 * @param date
+	 * @return
+	 */
+	public static Date getMonthEnd(Date date) {
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		//设置0秒
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.HOUR, 0);
+		cal.set(Calendar.AM_PM,Calendar.AM);
+		cal.set(Calendar.DAY_OF_MONTH,1);
+		cal.add(Calendar.MONTH, 1);
+		cal.add(Calendar.SECOND, -1);
+		return cal.getTime();
+	}
+
+	
 }
